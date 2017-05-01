b5433f1982fd443e7fc72219541ce16eb8755ce7
TangJun <tang_jun2006@163.com>
2017-05-01 14:00:33 +0800

    新增

--- admin/src/main/java/cn/tz/www/admin/controller/MainController.java
+++ admin/src/main/java/cn/tz/www/admin/controller/MainController.java
@@ -7,73 +7,109 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 @Controller
 public class MainController extends WebMvcConfigurerAdapter {
 	@RequestMapping("/")
-	public String index(Principal principal){
-		if(principal==null){
+	public String index(Principal principal) {
+		if (principal == null) {
 			return "redirect:/login";
 		}
 		return "index";
-		
+
 	}
+
 	@RequestMapping("/roleManage")
-	public String roleManage(Principal principal,HttpServletResponse response){
-		if(principal==null){
+	public String roleManage(Principal principal, HttpServletResponse response) {
+		if (principal == null) {
 			return "redirect:/login";
 		}
 		response.setHeader("X-Frame-Options", "SAMEORIGIN");
 		return "role_manage";
-		
+
 	}
+
 	@RequestMapping("/releaseInfos")
-	public String releaseInfos(Principal principal,HttpServletResponse response){
-		if(principal==null){
+	public String releaseInfos(Principal principal, HttpServletResponse response) {
+		if (principal == null) {
 			return "redirect:/login";
 		}
 		response.setHeader("X-Frame-Options", "SAMEORIGIN");
 		return "release_infos";
-		
+
 	}
+
 	@RequestMapping("/releaseNews")
-	public String releaseNews(Principal principal,HttpServletResponse response){
-		if(principal==null){
+	public String releaseNews(Principal principal, HttpServletResponse response) {
+		if (principal == null) {
 			return "redirect:/login";
 		}
 		response.setHeader("X-Frame-Options", "SAMEORIGIN");
 		return "release_news";
-		
+
 	}
+
 	@RequestMapping("/releaseSlide")
-	public String releaseSlide(Principal principal,HttpServletResponse response){
-		if(principal==null){
+	public String releaseSlide(Principal principal, HttpServletResponse response) {
+		if (principal == null) {
 			return "redirect:/login";
 		}
-		
-//	  response.getHeader("X-Frame-Options").
-		//response.addHeader("X-Frame-Options", "SAMEORIGIN");
+
+		// response.getHeader("X-Frame-Options").
+		// response.addHeader("X-Frame-Options", "SAMEORIGIN");
 		response.setHeader("X-Frame-Options", "SAMEORIGIN");
 		return "release_slide";
-		
-		
+
 	}
+
 	@RequestMapping("/usersManage")
-	public String usersManage(Principal principal,HttpServletResponse response){
-		if(principal==null){
+	public String usersManage(Principal principal, HttpServletResponse response) {
+		if (principal == null) {
 			return "redirect:/login";
 		}
 		response.setHeader("X-Frame-Options", "SAMEORIGIN");
 		return "users_manage";
-		
+
 	}
+
 	@RequestMapping("/usersAuthority")
-	public String usersAuthority(Principal principal,HttpServletResponse response){
-		if(principal==null){
+	public String usersAuthority(Principal principal, HttpServletResponse response) {
+		if (principal == null) {
 			return "redirect:/login";
 		}
 		response.setHeader("X-Frame-Options", "SAMEORIGIN");
 		return "permission_control";
-		
+
 	}
-	
+
+	@RequestMapping("/customerManage")
+	public String customerManage(Principal principal, HttpServletResponse response) {
+		if (principal == null) {
+			return "redirect:/login";
+		}
+		response.setHeader("X-Frame-Options", "SAMEORIGIN");
+		return "customer_manage";
+
+	}
+
+	@RequestMapping("/customerRole")
+	public String customerRole(Principal principal, HttpServletResponse response) {
+		if (principal == null) {
+			return "redirect:/login";
+		}
+		response.setHeader("X-Frame-Options", "SAMEORIGIN");
+		return "customer_role";
+
+	}
+
+	@RequestMapping("/customerPermission.")
+	public String customerPermission(Principal principal, HttpServletResponse response) {
+		if (principal == null) {
+			return "redirect:/login";
+		}
+		response.setHeader("X-Frame-Options", "SAMEORIGIN");
+		return "customer_permission";
+
+	}
+
 }
