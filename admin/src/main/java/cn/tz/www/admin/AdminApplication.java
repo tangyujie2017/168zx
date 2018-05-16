1ab570eb3bbb28bc4dc3a578ee59ddee6e05e3bf
tangjun <tang_jun2006@163.com>
2018-05-16 09:06:56 +0800

    测试

--- admin/src/main/java/cn/tz/www/admin/AdminApplication.java
+++ admin/src/main/java/cn/tz/www/admin/AdminApplication.java
@@ -9,6 +9,7 @@ import cn.tz.www.customer.Core;
 @SpringBootApplication
 public class AdminApplication {
 	public static void main(String[] args) {
+		
 		SpringApplication.run(AdminApplication.class, args);
 	}
 }
